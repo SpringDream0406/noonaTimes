6ae1b3180dfe92261f8680fb6768600acc41fbc0
SpringDream0406 <SpringDream0406@gmail.com>
2024-02-19 18:05:03 +0900

    누나 api test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = `bd25ebe1582a4199b54a3b6cc16784bf`;
 const newsAPI_url = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=`;
 const noonaAPI_url = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
-const netlify_url = `https://noonanews.netlify.app/`;
+const netlify_url = `https://noonanews.netlify.app/top-headlines?country=kr`;
 let news = [];
 
 const getLatestNes = async () => {
