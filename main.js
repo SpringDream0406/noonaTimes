2511c82a9025026e842317379ae73407bce30d15
SpringDream0406 <SpringDream0406@gmail.com>
2024-02-19 17:50:04 +0900

    api받기까지

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const API_KEY = `bd25ebe1582a4199b54a3b6cc16784bf`;
+const newsAPI_url = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=`;
+const noonaAPI_url = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
 let news = [];
 
 const getLatestNes = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+    `${noonaAPI_url}`
   );
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
+  console.log(news);
 };
 
 getLatestNes();
